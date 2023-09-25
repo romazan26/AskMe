bd210f00d290763ee03497c061cb2becfd477052
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-09-25 21:23:54 +0300

    Initial Commit

---
+++ AskMe/AskMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskMeApp.swift
+//  AskMe
+//
+//  Created by Роман on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AskMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AskMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AskMe
+//
+//  Created by Роман on 25.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
