4e43a29b281eec0a1cd16aae0198dbb9953ae5f0
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-09-25 22:26:47 +0300

    ferstView

--- AskMe/ContentView.swift
+++ AskMe/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var textAsk = ""
+    @State private var textAnswer = ""
+    @FocusState private var nameIsFocused: Bool
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+             Image(systemName: "bell")
+                .resizable()
+                .frame(width: 250, height: 250)
+            Text("Задай вопрос на который можно ответь ДА или НЕТ")
+                .font(.title)
+                .padding(.bottom, 50)
+            HStack {
+                TextField("Вопрос", text: $textAsk)
+                    .font(.title)
+                    .focused($nameIsFocused)
+                Button(action: randomAnswer, label: {
+                    Text("Ответ")
+                        .font(.title)
+                })
+                .buttonStyle(.borderedProminent)
+            }
+            
+            Text(textAnswer)
+                .font(.title)
+                .foregroundColor(.red)
+                .background(.yellow)
+                .cornerRadius(10)
         }
         .padding()
+    }
+    private func randomAnswer() {
+        nameIsFocused = false
+        var answerNumber = Int.random(in: 1...3)
+        textAsk = ""
+        if answerNumber == 1 {
+            textAnswer = "ДА"
+        } else if answerNumber == 2 {
+            textAnswer = "НЕТ"
+        } else {
+            textAnswer = "Возможно"
+        }
     }
 }
 
